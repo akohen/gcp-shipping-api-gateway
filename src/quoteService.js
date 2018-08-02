8e140cd18d8ede1145f14d1dd1bd6e772409dfe2
Alexandre KOHEN <alexandre@kohen.fr>
2018-08-02 17:47:50 +0200

    IAP connection to pricing service

--- src/quoteService.js
+++ src/quoteService.js
@@ -1,10 +1,25 @@
-import fetch from 'node-fetch';
+import { JWT } from 'google-auth-library';
+import keys from '../jwt.keys.json';
 
 const quoteService = {
-  getQuote: ({ from, to, weight }) =>
-    fetch(`${process.env.PRICING_SERVICE}/quote/${from}-${to}-${weight}`, { timeout: 1500, redirect: 'manual', follow: 0 })
-      .then(response => response.json())
-      .then(json => json.price),
+  getQuote: async ({ from, to, weight }) => {
+    const client = new JWT({
+      email: keys.client_email,
+      key: keys.private_key,
+      additionalClaims: { target_audience: process.env.PRICING_SERVICE_CLIENT_ID },
+    });
+    try {
+      const res = await client.request({ url: `${process.env.PRICING_SERVICE}/quote/${from}-${to}-${weight}` });
+      return res.data.price;
+    } catch (err) {
+      console.error(err.message);
+      if (err.code === '400') {
+        throw new Error('No quote returned');
+      } else {
+        throw new Error('Server error!!');
+      }
+    }
+  },
 };
 
 export default quoteService;
